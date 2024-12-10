8a9e5086d6b23b86e5195cf22259d692e83ad0a9
antxnLS <antxnlasalle@gmail.com>
2024-12-10 08:43:27 +0100

    Add files via upload

---
+++ ProjecteVariables/ProjecteVariables/ProjecteVariables.cpp
@@ -0,0 +1,36 @@
+// ProjecteVariables.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Valor;
+    int n1;
+    int n2;
+    Valor = 10;
+    cin >> n1;
+    cin >> n2;
+    cout << "Hola Mon!" <<endl<< Valor <<endl;
+    cout << "7 + 3 = " << n1 + n2 << endl;
+    cout << "7 - 3 = " << n1 - n2 << endl;
+    cout << "7 * 3 = " << n1 * n2 << endl;
+    cout << "7 / 3 = " << n1 / n2 << endl;
+    cout << "7.0 / 3.0 = " << n1 + n2 << endl;
+    cout << "7 % 3 = " << n1 % n2 << endl;
+    cout << "7 + 3 * 5= " << n1 + n2 * 5 << endl;
+    cout << "(7 + 3) * 5= " << (n1 + n2) * 5 << endl;
+return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
