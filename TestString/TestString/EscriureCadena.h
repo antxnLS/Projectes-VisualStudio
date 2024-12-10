85653cf9162b689204546ab482d5923c64c8edaa
antxnLS <antxnlasalle@gmail.com>
2024-12-10 11:28:53 +0100

    Delete TestString directory

--- TestString/TestString/EscriureCadena.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-using namespace std;
-#define MI_CADENA "Hola marti"
-
-char MEVA_CADENA[11] = { 'h','o','l','a',' ','r','o','b','e','r','t' };
-
-void EscriureCadena(char Cadena[]);
